355ae4861c4dee72d591531e093bc5a0c8257df9
Lieb_Math <liebmathieson2@gmai.com>
2022-06-28 18:16:14 -0400

    mod5 updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-import NavBar from './components/NavBar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from './components/pages/NavBar';
+import Profile from './components/pages/Profile';
 import Register from './components/RegisterForm';
 import Login from './components/LoginForm';
 import About from './components/About';
@@ -7,7 +9,17 @@ import About from './components/About';
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<NavBar />}>
+            <Route path="profile" element={<Profile />} />
+            <Route path="register" element={<Register />} />
+            <Route path="login" element={<Login />} />
+            <Route index element={<About />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      {/* <NavBar /> */}
       <About />
       <Login />
       <Register />
