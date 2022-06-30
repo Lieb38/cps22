8194e3e95559e64cf54e8a37adca2762f92aad4c
Lieb_Math <liebmathieson2@gmai.com>
2022-06-30 15:59:57 -0400

    where the hell did my index.js go????

---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,25 @@
+const Login = () => {
+    return(
+        <div>
+            
+            <div className="container">
+                <form className="login">
+                <h1>Login</h1>
+                 <div className="mx-4 my-4">
+                   <label for="exampleInputEmail1" className="form-label">Email address</label>
+                   <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                 </div>
+                 <div className="mx-4 my-4">
+                   <label for="exampleInputPassword1" className="form-label">Password</label>
+                   <input type="password" className="form-control" id="exampleInputPassword1"/>
+                 </div>
+                 <button type="submit" className="btn btn-primary my-4">Login</button>
+                </form>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default Login;
