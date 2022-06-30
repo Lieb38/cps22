86390eabc1ffc707a0604baa68ecf120e40f409a
Lieb_Math <liebmathieson2@gmai.com>
2022-06-30 15:15:06 -0400

    more edits m5

--- client/src/components/pages/NavBar.js
+++ client/src/components/pages/NavBar.js
@@ -5,6 +5,7 @@ const NavBar = () => {
         <div>
           <nav className="navbar navbar-expand-lg navbar-light">
             <div className="container-fluid">
+              <Link className="navbar-brand" to="/">Lieb Space</Link>
               <a className="navbar-brand" href="#">Liebook</a>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
@@ -40,6 +41,7 @@ const NavBar = () => {
                </div>
             </div>
           </nav>
+          <Outlet />
         </div>
     );
 }
---
+++ client/src/main.js
@@ -0,0 +1,14 @@
+export async function detchdata(route = '', data = {}, methodType) {
+    const response = await fetch(`http://localhost:5000${route}`, {
+        mothod: methodType, /// get, post, put, delete, etc.
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data) // body data must match content-type header
+    });
+    if(response.ok) {
+        return await response.json(); // parse JSON response int native JavaScript Objects
+    } else {
+        throw await response.json();
+    }
+}
