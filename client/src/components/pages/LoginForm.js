3434dbb4d0481c1c3f78879a8a40569d8c5b70dc
Lieb_Math <liebmathieson2@gmai.com>
2022-07-02 11:34:52 -0400

    more css changes

--- client/src/components/pages/LoginForm.js
+++ client/src/components/pages/LoginForm.js
@@ -2,9 +2,9 @@ const Login = () => {
     return(
         <div>
             
-            <div className="container">
+            <div className="container mw-30">
                 <form className="login">
-                <h1>Login</h1>
+                <h1>Log In</h1>
                  <div className="mx-4 my-4">
                    <label for="exampleInputEmail1" className="form-label">Email address</label>
                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
--- client/src/components/pages/RegisterForm.js
+++ client/src/components/pages/RegisterForm.js
@@ -6,14 +6,14 @@ const Register = () => {
                     <h1>Register</h1>
                 <div className="mx-4 my-4">
                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                   <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                   <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                  </div>
                  <div className="mx-4 my-4">
-                   <label for="exampleInputPassword1" class="form-label">Password</label>
-                   <input type="password" class="form-control" id="exampleInputPassword1"/>
+                   <label for="exampleInputPassword1" className="form-label">Password</label>
+                   <input type="password" className="form-control" id="exampleInputPassword1"/>
                  </div>
-                 <button type="submit" class="btn btn-primary my-4">Register</button>
+                 <button type="submit" className="btn my-4">Register</button>
                 </form>
             </div>
 
