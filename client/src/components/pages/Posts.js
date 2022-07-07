60d821072ee75a1d6998dae6b82cc52989858574
Lieb_Math <liebmathieson2@gmai.com>
2022-07-07 14:37:44 -0400

    edit test

--- client/src/components/pages/Posts.js
+++ client/src/components/pages/Posts.js
@@ -18,73 +18,46 @@ useEffect(() => {
   .then(data => setPosts(data))
 },[])
 
-// const deleteMe = (e, _id) => {
-//   e.preventDefault();
+const deleteMe = (e, _id) => {
+  e.preventDefault();
 
-//   fetchData('/post/delete', 
-//         {
-//         _id: _id
-//         }, 
-//       "DELETE")
-//       .then((data) => {
-//           if(!data.message) {
-//               console.log(_id)
-//               console.log(data)
-//               // console.log(`success!!"`)
-//           }
-//       })
-//       .catch((error) => {
-//           console.log(`Error! ${error.message}`)
-//       })
+  fetchData('/post/delete', 
+        {
+        _id: _id
+        }, 
+      "DELETE")
+      .then((data) => {
+          if(!data.message) {
+              console.log(_id)
+              console.log(data)
+              // console.log(`success!!"`)
+          }
+      })
+      .catch((error) => {
+          console.log(`Error! ${error.message}`)
+      })
 
 
-//     //   fetch("/getPosts") // http://localhost:5000/post
-//     // .then(response => response.json())
-//     // .then(data => setPosts(data))
-// }
+    //   fetch("/getPosts") // http://localhost:5000/post
+    // .then(response => response.json())
+    // .then(data => setPosts(data))
+}
 
     return(
       <div className="postsSection" id="postsSection">
 
-        {posts.map(post => {
-          const deleteMe = (e) => {
-            e.preventDefault();
-          
-            fetchData('/post/delete', 
-                  {
-                  post._id
-                  }, 
-                "DELETE")
-                .then((data) => {
-                    if(!data.message) {
-                        console.log(_id)
-                        console.log(data)
-                        // console.log(`success!!"`)
-                    }
-                })
-                .catch((error) => {
-                    console.log(`Error! ${error.message}`)
-                })
-          
-          
-              //   fetch("/getPosts") // http://localhost:5000/post
-              // .then(response => response.json())
-              // .then(data => setPosts(data))
-          }
-
-
-
-            return(
-                // <div key={post._id} id={`postContainer`} className='postContainer'>
-                //   <form className="Posties" onSubmit={deleteMe}>
-                //     {/* onClick={() => deleteMe(post._id)} */}
-                //     <h1 className="postyContent">{post.content}</h1>
-                //     <h2>{user.username}</h2>
-                //     <button type="submit" className="btn"  id='DeleteME' >Delete</button>
-                //   </form>
-                // </div>
-            )}
-        )}
+                {posts.map(post => {
+                    return(
+                        <div key={post._id} id={`postContainer`} className='postContainer'>
+                          <form className="Posties" onSubmit={(e) => deleteMe(e,post._id)}>
+                            {/* onClick={() => deleteMe(post._id)} */}
+                            <h1 className="postyContent">{post.content}</h1>
+                            <h2>{user.username}</h2>
+                            <button type="submit" className="btn"  id='DeleteME' >Delete</button>
+                          </form>
+                        </div>
+                    )}
+                )}
       </div>
       
 
